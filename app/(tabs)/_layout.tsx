936700ba8832d6d014ebdd593e15f92ae23458e7
caseBread <kgu0515@gmail.com>
2026-05-25 09:10:56 +0900

    feat : 커뮤니티 버튼 살리기

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -13,8 +13,8 @@ export default function TabLayout() {
       >
         <Tabs.Screen name="index" options={{ title: "홈" }} />
         <Tabs.Screen name="mountains" options={{ title: "산목록" }} />
-        <Tabs.Screen name="tracking" options={{ title: '기록' }} />
-        {/* <Tabs.Screen name="community" options={{ title: '커뮤니티' }} /> */}
+        <Tabs.Screen name="tracking" options={{ title: "기록" }} />
+        <Tabs.Screen name="community" options={{ title: "커뮤니티" }} />
         <Tabs.Screen name="mypage" options={{ title: "MY" }} />
       </Tabs>
     </HomeStateProvider>
