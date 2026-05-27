59c2c4154bc644344d36ff8d7f751c65830f2b11
Jio <giomong2346@naver.com>
2026-05-27 13:28:50 +0900

    fix: app.config.js에 withModularHeaders 플러그인 등록 누락 추가

--- app.config.js
+++ app.config.js
@@ -56,6 +56,7 @@ const config = {
   plugins: [
     "expo-router",
     "@react-native-firebase/app",
+    "./plugins/withModularHeaders",
     ...(isLiveActivityEnabled ? ["./plugins/withLiveActivity"] : []),
     [
       "expo-notifications",
